6334828ac4b23c5e0d22de01326394dc884d2c7a
HayatoYunoki <yunoki-hayato@g.ecc.u-tokyo.ac.jp>
2023-02-04 15:18:28 +0900

    init mdfsls

---
+++ mdfsla.py
@@ -0,0 +1,26 @@
+P = 200 #population size
+m = 10 #number of memeplexes
+iMax = 500 #number of iterations
+alpha = 0.4 #parameter for discrete
+pm = 0.06 #mutation rate
+
+
+class population:
+    memeplex_list = []
+    def __init__(self, luggage_num):
+        self.luggageNum = luggageNum
+    
+    def init_population():
+        for i in range(m):
+
+
+
+class memeplex:
+    def __init__(self):
+
+
+class individual:
+    def __init__(self):
+
+
+#main
